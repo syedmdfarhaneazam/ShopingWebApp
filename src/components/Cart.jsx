25a693fb52a9ba0c4182ff39ca8191ba280dbe1e
syedmdfarhaneazam <syedmdfarhaneazam@gmail.com>
2024-10-18 10:18:53 +0000

    Completed
    Learning about reducers with this project

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,9 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
-  const totalPrice = items.reduce(
+import { useContext } from "react"; //to access the context value
+import { CartContext } from "../store/contextsForCart";
+export default function Cart() {
+  //here we are gonna use the context value
+  const cartCtx = useContext(CartContext);
+  const totalPrice = cartCtx.items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
@@ -7,10 +11,10 @@ export default function Cart({ items, onUpdateItemQuantity }) {
 
   return (
     <div id="cart">
-      {items.length === 0 && <p>No items in cart!</p>}
-      {items.length > 0 && (
+      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
+      {cartCtx.items.length > 0 && (
         <ul id="cart-items">
-          {items.map((item) => {
+          {cartCtx.items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
@@ -20,11 +24,11 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => cartCtx.updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => cartCtx.updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
